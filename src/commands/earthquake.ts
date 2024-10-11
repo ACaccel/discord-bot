4351622dfa2fc4a70bc25e7a7051ab34ef1938a6
brian98077 <brian98077@yahoo.com.tw>
2024-10-12 00:54:48 +0800

    feat: earthquake warning magnitude > 4

--- src/commands/earthquake.ts
+++ src/commands/earthquake.ts
@@ -3,7 +3,11 @@ import { Channel } from "discord.js";
 
 export const earthquake_warning = async(channel: Channel, magnitude: number, countdown: number) => {
     channel = channel as AllowedTextChannel;
-    if (magnitude >= 2) {
+    if (magnitude >= 4) {
+        channel.send(`# <@&1235653362482024569> 有感地震警報，台北預估震度${magnitude}級，${countdown}秒後抵達！！！` +
+        "\n> 肥貓跌倒了！！");
+    }
+    else if (magnitude >= 2) {
         channel.send(`# <@&1235653362482024569> 有感地震警報，台北預估震度${magnitude}級，${countdown}秒後抵達！！！` +
         "\n> 為什麼要地震！？");
         // channel.send(`# 有感地震警報，台北預估震度${magnitude}級，${countdown}秒後抵達！！！` +
