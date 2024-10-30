579a35f28c8a07a0d237091d2da78421bb3af140
ACaccel <c8763318@gmail.com>
2024-10-30 08:08:36 +0000

    fix: earth quake notification threshold

--- src/commands/earthquake.ts
+++ src/commands/earthquake.ts
@@ -3,10 +3,10 @@ import { Channel } from "discord.js";
 
 export const earthquake_warning = async(channel: Channel, eq_role: string, magnitude: number, countdown: number) => {
     channel = channel as AllowedTextChannel;
-    if (magnitude >= 4) {
+    if (magnitude >= 5) {
         channel.send(`# <@&${eq_role}> 有感地震警報，台北預估震度${magnitude}級，${countdown}秒後抵達！！！` +
         "\n> 肥貓跌倒了！！");
-    } else if (magnitude >= 2) {
+    } else if (magnitude >= 3) {
         channel.send(`# <@&${eq_role}> 有感地震警報，台北預估震度${magnitude}級，${countdown}秒後抵達！！！` +
         "\n> 為什麼要地震！？");
         // channel.send(`# 有感地震警報，台北預估震度${magnitude}級，${countdown}秒後抵達！！！` +
