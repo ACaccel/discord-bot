33bed454183957c3d8d6eb89b1d0587268dcaff6
ACaccel <c8763318@gmail.com>
2025-01-20 13:24:45 +0000

    refactor: update message backup

--- src/bot/msg_archive/types.ts
+++ src/bot/msg_archive/types.ts
@@ -1,4 +1,4 @@
-import { Client } from 'discord.js';
+import { Client, GuildMember } from 'discord.js';
 import {
     AllowedTextChannel,
     BaseBot,
@@ -16,88 +16,93 @@ export class MsgArchive extends BaseBot {
         this.msgArchiveConfig = msgArchiveConfig as MsgArchiveConfig;
     }
 
-    public messageBackup = (guild_id: string, minute: number) => {
+    public messageBackup = async (guild_id: string, minute: number) => {
+        await this.backup(guild_id);
         setInterval(async () => {
-            try {
-                var begin = Date.now();
-                var newMessageCnt = 0;
-                const totalMessageCnt = await db.Message.countDocuments({});
-                const debug_ch = this.guildInfo[guild_id].channels.debug as AllowedTextChannel;
-                const sentMessage = await debug_ch.send(`[ SYSTEM ] on scheduled backup process. The database now contains ( ${totalMessageCnt}+${newMessageCnt} ) messages.`);
-                const fetchPromise = this.guildInfo[guild_id].guild.channels.cache.map(async(channel) => {
-                    // check if channel is already in database
-                    const channelName = channel.name;
-                    var lastMessageQuery = await db.Fetch.findOne({channel: channelName, channelID: channel.id});
-                    if(lastMessageQuery === null) {
-                        const lastMessage = new db.Fetch({
+            await this.backup(guild_id);
+        }, minute * 60 * 1000);
+    }
+
+    public backup = async (guild_id: string) => {
+        try {
+            var begin = Date.now();
+            var newMessageCnt = 0;
+            const totalMessageCnt = await db.Message.countDocuments({});
+            const debug_ch = this.guildInfo[guild_id].channels.debug as AllowedTextChannel;
+            const sentMessage = await debug_ch.send(`[ SYSTEM ] on scheduled backup process. The database now contains ( ${totalMessageCnt}+${newMessageCnt} ) messages.`);
+            const fetchPromise = this.guildInfo[guild_id].guild.channels.cache.map(async(channel) => {
+                // check if channel is already in database
+                const channelName = channel.name;
+                var lastMessageQuery = await db.Fetch.findOne({channel: channelName, channelID: channel.id});
+                if(lastMessageQuery === null) {
+                    const lastMessage = new db.Fetch({
+                        channel: channelName,
+                        channelID:channel.id,
+                        lastMessageID: 0
+                    })
+                    await lastMessage.save();
+                }
+                
+                // fetch messages
+                var lastID = (await db.Fetch.findOne({channel: channel.name, channelID: channel.id}))?.lastMessageID;
+                if (!channel.isTextBased()) return;
+                const fetchedMessages = await channel.messages.fetch({ 
+                    limit: 100, 
+                    ...(lastID && { after: lastID }) 
+                });
+
+                if(fetchedMessages.size === 0) {
+                    await sentMessage.edit(`[ SYSTEM ] end scheduled backup process. The database now contains ( ${totalMessageCnt}+${newMessageCnt} ) messages.`);
+                    return;
+                }
+                
+                lastID = fetchedMessages.firstKey();
+                await db.Fetch.findOneAndUpdate({channel: channel.name, channelID: channel.id}, {lastMessageID: lastID});
+                const allMessages = fetchedMessages
+
+                // save messages
+                for(const f of allMessages) {
+                    const e = f[1];
+                    const channelID = channel.id;
+                    const content = e.content;
+                    const userID = e.author.id;
+                    const username = e.author.username;
+                    const messageID = e.id;
+                    const timestamp = e.createdTimestamp;
+                    const exists = await db.Message.find({
+                        userID: userID, 
+                        username: username, 
+                        channel: channelName, 
+                        channelID: channelID,
+                        content: content, 
+                        messageID: messageID,
+                        timestamp: timestamp
+                    })
+                    if(exists.length === 0 && content !== "") {
+                        const newMessage = new db.Message({
                             channel: channelName,
-                            channelID:channel.id,
-                            lastMessageID: 0
-                        })
-                        await lastMessage.save();
-                    }
-                    
-                    // fetch messages
-                    var lastID = (await db.Fetch.findOne({channel: channel.name, channelID: channel.id}))?.lastMessageID;
-                    if (!channel.isTextBased()) return;
-                    const fetchedMessages = await channel.messages.fetch({ 
-                        limit: 100, 
-                        ...(lastID && { after: lastID }) 
-                    });
-
-                    if(fetchedMessages.size === 0) {
-                        await sentMessage.edit(`[ SYSTEM ] end scheduled backup process. The database now contains ( ${totalMessageCnt}+${newMessageCnt} ) messages.`);
-                        return;
-                    }
-                    
-                    lastID = fetchedMessages.firstKey();
-                    await db.Fetch.findOneAndUpdate({channel: channel.name, channelID: channel.id}, {lastMessageID: lastID});
-                    const allMessages = fetchedMessages
-
-                    // save messages
-                    for(const f of allMessages) {
-                        const e = f[1];
-                        const channelID = channel.id;
-                        const content = e.content;
-                        const userID = e.author.id;
-                        const username = e.author.username;
-                        const messageID = e.id;
-                        const timestamp = e.createdTimestamp;
-                        const exists = await db.Message.find({
-                            userID: userID, 
-                            username: username, 
-                            channel: channelName, 
                             channelID: channelID,
-                            content: content, 
+                            content: content,
+                            userID: userID,
+                            username: username,
                             messageID: messageID,
                             timestamp: timestamp
                         })
-                        if(exists.length === 0 && content !== "") {
-                            const newMessage = new db.Message({
-                                channel: channelName,
-                                channelID: channelID,
-                                content: content,
-                                userID: userID,
-                                username: username,
-                                messageID: messageID,
-                                timestamp: timestamp
-                            })
-                            await newMessage.save();
-                            newMessageCnt++;
-                            if(newMessageCnt % 1000 == 0) {
-                                await sentMessage.edit(`[ SYSTEM ] on scheduled backup process. The database now contains ( ${totalMessageCnt}+${newMessageCnt} ) messages.`);
-                            }
+                        await newMessage.save();
+                        newMessageCnt++;
+                        if(newMessageCnt % 1000 == 0) {
+                            await sentMessage.edit(`[ SYSTEM ] on scheduled backup process. The database now contains ( ${totalMessageCnt}+${newMessageCnt} ) messages.`);
                         }
                     }
-                });
-                await Promise.all(fetchPromise);
-                var end = Date.now();
-                var timeSpent = (end-begin) / 1000;
-                await sentMessage.edit(`[ SYSTEM ] end scheduled backup process. The database now contains ( ${totalMessageCnt}+${newMessageCnt} ) messages. (${timeSpent} sec)`);
-            } catch(e) {
-                utils.errorLogger(this.clientId, e);
-            }
-        }, minute * 60 * 1000);
+                }
+            });
+            await Promise.all(fetchPromise);
+            var end = Date.now();
+            var timeSpent = (end-begin) / 1000;
+            await sentMessage.edit(`[ SYSTEM ] end scheduled backup process. The database now contains ( ${totalMessageCnt}+${newMessageCnt} ) messages. (${timeSpent} sec)`);
+        } catch(e) {
+            utils.errorLogger(this.clientId, e);
+        }
     }
 }
 
