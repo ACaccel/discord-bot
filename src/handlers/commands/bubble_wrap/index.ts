0fc1ae16e5881357f6f85a1217e9c0be6011c953
neverlate1457 <neverlatebang@gmail.com>
2025-12-17 17:12:45 +0800

    Refactor bubble_wrap to use SlashCommand
    modify char width

--- src/handlers/commands/bubble_wrap/index.ts
+++ src/handlers/commands/bubble_wrap/index.ts
@@ -2,9 +2,9 @@ import {
     ChatInputCommandInteraction,
 } from 'discord.js';
 import { BaseBot } from '@bot';
-import { Command } from '@cmd';
+import { SlashCommand } from '@cmd';
 
-export default class bubble_wrap extends Command {
+export default class bubble_wrap extends SlashCommand {
     constructor() {
         super();
         this.setConfig({
@@ -21,10 +21,15 @@ export default class bubble_wrap extends Command {
             }
         });
     }
+    private getVisualWidth(char: string): number {
+    
+        return /[^\x00-\xff]/.test(char) ? 2 : 1;
+    }
 
     public override async execute(interaction: ChatInputCommandInteraction, bot: BaseBot): Promise<void> {
         const inner_str = interaction.options.get("str")?.value as string;
         const side_len = 7;
+        const total_cells = side_len * side_len;
         if (inner_str.length > side_len * side_len) {
             await interaction.reply({ content: "字串太長了，請縮短到 64 字元以內" });
             return;
@@ -39,8 +44,20 @@ export default class bubble_wrap extends Command {
 
         // fill the board with the inner_str
         const board = Array(side_len * side_len).fill("||<:blank:1082500408838205540>||");
+        
         for (let i = 0; i < inner_str.length; i++) {
-            board[places[i]] = "||" + inner_str[i] + "||";
+            const char = inner_str[i];
+            
+            //modify width 
+        
+            let displayChar = char;
+            if (this.getVisualWidth(char) === 1) {
+                displayChar = ` ${char}  `; 
+            }else{
+                displayChar = `${char} `;
+            }
+            
+            board[places[i]] = "||" + displayChar + "||";
         }
 
         // create the string representation of the board
