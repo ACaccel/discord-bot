bc2939d6bc0c504c6765bdfd617f2687cc2d3c98
ACaccel <c8763318@gmail.com>
2025-12-27 15:19:46 +0000

    fix: update earthquake warning message to specify magnitude level

--- src/events/earthquake.ts
+++ src/events/earthquake.ts
@@ -2,6 +2,6 @@ import { Channel } from "discord.js";
 
 export const earthquake_warning = async(channel: Channel, eq_role: string) => {
     if (!channel.isSendable()) return;
-    await channel.send(`# <@&${eq_role}> 台北強震警報!!!` +
+    await channel.send(`# <@&${eq_role}> 台北強震警報!!! (4級以上)` +
         "\n> 為什麼要地震！？");
 }
